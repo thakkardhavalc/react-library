92e03b825eb247b9fbe5c1774803201dde008c37
thakkardhavalc <thakkardhavalc@gmail.com>
2024-03-05 10:00:29 -0700

    implemented chaange quantity options under admin panel

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -44,6 +44,13 @@ export const Navbar = () => {
                                 </NavLink>
                             </li>
                         }
+                        {authState.isAuthenticated && authState.accessToken?.claims?.userType === 'admin' &&
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/admin">
+                                    Admin
+                                </NavLink>
+                            </li>
+                        }
                     </ul>
                     <ul className='navbar-nav ms-auto'>
                         {!authState.isAuthenticated ?
